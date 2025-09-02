3f956a44925c04530ebfb8f87bfa6ee95e0bb6cd
bascom241 <135514268+bascom241@users.noreply.github.com>
2025-09-02 12:19:44 +0100

    Dashboard Implementation Began

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import SignIn from './pages/auth/SignIn'
 import Layout from './components/Layout'
 import Dashboard from './pages/dashboard/Dashboard'
@@ -12,21 +12,26 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/register" element={<Register />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
 
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
+      <BrowserRouter basename='/app'>
 
-        </Route>
-      </Routes>
+
+        <Routes>
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+
+          <Route element={<Layout />}>
+            <Route path="/dashboard" element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
